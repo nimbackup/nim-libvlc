496700d85d2b1c94f3db18022052c75bcf2f55eb
Danil Yarantsev <21169548+Yardanico@users.noreply.github.com>
2020-05-13 17:04:59 +0300

    Remove unneeded overrides

--- src/libvlc.nim
+++ src/libvlc.nim
@@ -55,27 +55,6 @@ cOverride:
     video_setup_device_info_t* {.bycopy.} = object
       ano_tt_11*: INNER_C_UNION_tt_4
 
-    INNER_C_UNION_ff_4* {.bycopy, union.} = object
-      dxgi_format*: cint         ## * the rendering DXGI_FORMAT for \ref libvlc_video_engine_d3d11
-      d3d9_format*: uint32     ## * the rendering D3DFORMAT for \ref libvlc_video_engine_d3d9
-      opengl_format*: cint ## * the rendering GLint GL_RGBA or GL_RGB for \ref libvlc_video_engine_opengl and
-                        ##                              for \ref libvlc_video_engine_gles2
-      p_surface*: pointer        ## * currently unused
-
-    video_output_cfg_t* {.bycopy.} = object
-      ano_ff_9*: INNER_C_UNION_ff_4
-      full_range*: bool          ## * video is full range or studio/limited range
-      colorspace*: video_color_space_t ## * video color space
-      primaries*: video_color_primaries_t ## * video color primaries
-      transfer*: video_transfer_func_t ## * video transfer function
-
-  const
-    teletext_key_red = ord('r') shl 16
-    teletext_key_green = ord('g') shl 16
-    teletext_key_yellow = ord('b') shl 16
-    teletext_key_blue = ord('y') shl 16
-    teletext_key_index = ord('i') shl 16
-
 cImport(
   baseDir / "include" / "vlc" / "vlc.h", 
   recurse = true, dynlib = "dynvlc", flags = "-f:ast2"
